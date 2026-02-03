a6b27c53fe1b215cb2ce27afdfb6c48e6fe20bbb
gws <glennws@pm.me>
2026-02-03 11:53:03 +0100

    fixlay

--- src/lib.rs
+++ src/lib.rs
@@ -1,110 +1,450 @@
-use std::{default, io, num::NonZeroU8};
+use std::{fmt::Display, num::NonZeroU8};
 
-use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind, MouseEvent};
+use crossterm::event::KeyModifiers;
+use itertools::Itertools;
 use ratatui::{
     DefaultTerminal, Frame,
-    buffer::Buffer,
-    layout::{Constraint, HorizontalAlignment, Layout, Rect},
-    style::Stylize,
-    symbols::border,
-    text::{Line, Text},
-    widgets::{Block, List, Paragraph, Table, TableState, Widget},
+    crossterm::event::{self, Event, KeyCode, KeyEventKind},
+    layout::{Constraint, Layout, Margin, Rect},
+    style::{self, Color, Modifier, Style, Stylize},
+    text::Text,
+    widgets::{
+        Block, BorderType, Cell, HighlightSpacing, Paragraph, Row, Scrollbar, ScrollbarOrientation,
+        ScrollbarState, Table, TableState,
+    },
 };
+use style::palette::tailwind;
+use unicode_width::UnicodeWidthStr;
 
-type Row = [Option<NonZeroU8>; 9];
-type Mat9x9 = [Row; 9];
+const PALETTES: [tailwind::Palette; 4] = [
+    tailwind::BLUE,
+    tailwind::EMERALD,
+    tailwind::INDIGO,
+    tailwind::RED,
+];
+const INFO_TEXT: [&str; 2] = [
+    "(Esc) quit | (↑) move up | (↓) move down | (←) move left | (→) move right",
+    "(Shift + →) next color | (Shift + ←) previous color",
+];
 
-#[derive(Debug, Default)]
-pub struct App {
-    grid: Mat9x9,
-    exit: bool,
-    grid_state: TableState,
+const ITEM_HEIGHT: usize = 4;
+
+struct TableColors {
+    buffer_bg: Color,
+    header_bg: Color,
+    header_fg: Color,
+    row_fg: Color,
+    selected_row_style_fg: Color,
+    selected_column_style_fg: Color,
+    selected_cell_style_fg: Color,
+    normal_row_color: Color,
+    alt_row_color: Color,
+    footer_border_color: Color,
 }
 
-impl App {
-    pub fn run(&mut self, term: &mut DefaultTerminal) -> io::Result<()> {
-        while !self.exit {
-            term.draw(|frame| self.draw(frame))?;
-            self.handle_events()?
+impl TableColors {
+    const fn new(color: &tailwind::Palette) -> Self {
+        Self {
+            buffer_bg: tailwind::SLATE.c950,
+            header_bg: color.c900,
+            header_fg: tailwind::SLATE.c200,
+            row_fg: tailwind::SLATE.c200,
+            selected_row_style_fg: color.c400,
+            selected_column_style_fg: color.c400,
+            selected_cell_style_fg: color.c600,
+            normal_row_color: tailwind::SLATE.c950,
+            alt_row_color: tailwind::SLATE.c900,
+            footer_border_color: color.c400,
         }
-        Ok(())
+    }
+}
+
+#[derive(Default, Debug, Clone, Copy)]
+struct CellData(Option<NonZeroU8>);
+
+impl From<u8> for CellData {
+    fn from(value: u8) -> Self {
+        match value {
+            0 => Self(None),
+            v @ 1..=9 => Self(NonZeroU8::new(v)),
+            10.. => panic!("max value in soduku is 9"),
+        }
+    }
+}
+
+impl Display for CellData {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self.0 {
+            Some(v) => write!(f, "{}", v),
+            None => write!(f, "0"),
+        }
+    }
+}
+
+type Arr9 = [CellData; 9];
+type Grid9x9 = [Arr9; 9];
+
+struct Data {
+    name: String,
+    address: String,
+    email: String,
+    row: Arr9,
+}
+
+impl Data {
+    const fn ref_array(&self) -> [&String; 3] {
+        [&self.name, &self.address, &self.email]
     }
 
-    fn render_table(&mut self, frame: &mut Frame, area: Rect) {
-        let rows = (1..=9);
+    fn name(&self) -> &str {
+        &self.name
+    }
+
+    fn address(&self) -> &str {
+        &self.address
+    }
+
+    fn email(&self) -> &str {
+        &self.email
+    }
+}
+
+pub struct App {
+    state: TableState,
+    items: Vec<Data>,
+    longest_item_lens: (u16, u16, u16), // order is (name, address, email)
+    scroll_state: ScrollbarState,
+    colors: TableColors,
+    color_index: usize,
+}
+
+pub type Result = color_eyre::Result<()>;
+
+impl App {
+    pub fn new() -> Self {
+        let data_vec = generate_fake_names();
+        Self {
+            state: TableState::default().with_selected(0),
+            longest_item_lens: constraint_len_calculator(&data_vec),
+            scroll_state: ScrollbarState::new((data_vec.len() - 1) * ITEM_HEIGHT),
+            colors: TableColors::new(&PALETTES[0]),
+            color_index: 0,
+            items: data_vec,
+        }
+    }
+    pub fn next_row(&mut self) {
+        let i = match self.state.selected() {
+            Some(i) => {
+                if i >= self.items.len() - 1 {
+                    0
+                } else {
+                    i + 1
+                }
+            }
+            None => 0,
+        };
+        self.state.select(Some(i));
+        self.scroll_state = self.scroll_state.position(i * ITEM_HEIGHT);
+    }
+
+    pub fn previous_row(&mut self) {
+        let i = match self.state.selected() {
+            Some(i) => {
+                if i == 0 {
+                    self.items.len() - 1
+                } else {
+                    i - 1
+                }
+            }
+            None => 0,
+        };
+        self.state.select(Some(i));
+        self.scroll_state = self.scroll_state.position(i * ITEM_HEIGHT);
+    }
+
+    pub fn next_column(&mut self) {
+        self.state.select_next_column();
+    }
+
+    pub fn previous_column(&mut self) {
+        self.state.select_previous_column();
+    }
+
+    pub fn next_color(&mut self) {
+        self.color_index = (self.color_index + 1) % PALETTES.len();
+    }
+
+    pub fn previous_color(&mut self) {
+        let count = PALETTES.len();
+        self.color_index = (self.color_index + count - 1) % count;
+    }
+
+    pub fn set_colors(&mut self) {
+        self.colors = TableColors::new(&PALETTES[self.color_index]);
+    }
+
+    pub fn run(mut self, mut terminal: DefaultTerminal) -> Result {
+        loop {
+            terminal.draw(|frame| self.draw(frame))?;
+
+            if let Event::Key(key) = event::read()? {
+                if key.kind == KeyEventKind::Press {
+                    let shift_pressed = key.modifiers.contains(KeyModifiers::SHIFT);
+                    match key.code {
+                        KeyCode::Char('q') | KeyCode::Esc => return Ok(()),
+                        KeyCode::Char('j') | KeyCode::Down => self.next_row(),
+                        KeyCode::Char('k') | KeyCode::Up => self.previous_row(),
+                        KeyCode::Char('l') | KeyCode::Right if shift_pressed => self.next_color(),
+                        KeyCode::Char('h') | KeyCode::Left if shift_pressed => {
+                            self.previous_color();
+                        }
+                        KeyCode::Char('l') | KeyCode::Right => self.next_column(),
+                        KeyCode::Char('h') | KeyCode::Left => self.previous_column(),
+                        KeyCode::Char(c) if c.is_digit(10) => {
+                            let Some((r, col)) = self.state.selected_cell() else {
+                                continue;
+                            };
+                            self.items[r].row[col] =
+                                c.to_digit(10).map(|d| d as u8).unwrap().into();
+                        }
+
+                        _ => {}
+                    }
+                }
+            }
+        }
     }
 
     fn draw(&mut self, frame: &mut Frame) {
-        // let area = frame.area();
+        let vertical = Layout::vertical([
+            Constraint::Fill(1),
+            Constraint::Length(9 + 2 + 2),
+            Constraint::Fill(1),
+        ]);
+        let vertical_areas = vertical.split(frame.area());
+        let grid_row = Layout::horizontal([
+            Constraint::Fill(1),
+            Constraint::Length(4 * 9),
+            Constraint::Fill(1),
+        ])
+        .vertical_margin(1)
+        .split(vertical_areas[1]);
 
-        // let table = Table::new(rows, widths);
+        self.set_colors();
 
-        // for (i, cell) in cells.enumerate() {
-        //     Paragraph::new(format!("{i}")).render(cell, buf);
-        // }
-    }
-    fn handle_key_event(&mut self, key_event: KeyEvent) {
-        match key_event.code {
-            KeyCode::Char('q') => self.exit(),
-            KeyCode::Left => self.decrement(),
-            KeyCode::Right => self.increment(),
-            _ => {}
-        }
-    }
-    fn handle_mouse_event(&mut self, event: MouseEvent) {
-        match event.kind {
-            event::MouseEventKind::Down(mouse_button) => {}
-            // event::MouseEventKind::Up(mouse_button) => todo!(),
-            // event::MouseEventKind::Drag(mouse_button) => todo!(),
-            // event::MouseEventKind::Moved => todo!(),
-            // event::MouseEventKind::ScrollDown => todo!(),
-            // event::MouseEventKind::ScrollUp => todo!(),
-            // event::MouseEventKind::ScrollLeft => todo!(),
-            // event::MouseEventKind::ScrollRight => todo!(),
-            _ => {}
-        }
-        todo!()
+        self.render_header(frame, vertical_areas[0]);
+        self.render_table(frame, grid_row[1]);
+        // self.render_scrollbar(frame, rects[0]);
+        self.render_footer(frame, vertical_areas[2]);
     }
 
-    fn handle_events(&mut self) -> io::Result<()> {
-        match event::read()? {
-            Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
-                self.handle_key_event(key_event)
+    fn render_table(&mut self, frame: &mut Frame, area: Rect) {
+        let header_style = Style::default()
+            .fg(self.colors.header_fg)
+            .bg(self.colors.header_bg);
+        let selected_row_style = Style::default()
+            .add_modifier(Modifier::REVERSED)
+            .fg(self.colors.selected_row_style_fg);
+        let selected_col_style = Style::default().fg(self.colors.selected_column_style_fg);
+        let selected_cell_style = Style::default()
+            .add_modifier(Modifier::REVERSED)
+            .fg(self.colors.selected_cell_style_fg);
+
+        let rows = self.items.iter().enumerate().map(|(r, data)| {
+            let color = match r % 2 {
+                0 => self.colors.normal_row_color,
+                _ => self.colors.alt_row_color,
+            };
+            let base_style = Style::new().fg(self.colors.row_fg).bg(color);
+            let underline = (r + 1) % 3 == 0 && (r + 1) < 9;
+            let style = if underline {
+                // base_style.add_modifier(Modifier::UNDERLINED)
+                base_style
+            } else {
+                base_style
+            };
+            data.row
+                .into_iter()
+                .enumerate()
+                .map(|(col, content)| {
+                    let mut text = Text::from(format!("{content}"));
+                    if (col + 1) % 3 == 0 && (col + 1) < 9 {
+                        text.push_span(" |");
+                        text = text.right_aligned();
+                    } else {
+                        text = text.centered();
+                    }
+                    if underline {
+                        text.push_line("----");
+                    }
+                    Cell::from(text)
+                })
+                .collect::<Row>()
+                .style(style)
+                .height(if underline { 2 } else { 1 })
+        });
+        let bar = " █ ";
+        let t = Table::new(
+            rows,
+            [
+                // + 1 is for padding.
+                // Constraint::Length(self.longest_item_lens.0 + 1),
+                // Constraint::Min(self.longest_item_lens.1 + 1),
+                // Constraint::Min(self.longest_item_lens.2),
+                Constraint::Length(4),
+                Constraint::Length(4),
+                Constraint::Length(4),
+                //
+                Constraint::Length(4),
+                Constraint::Length(4),
+                Constraint::Length(4),
+                //
+                Constraint::Length(4),
+                Constraint::Length(4),
+                Constraint::Length(4),
+            ],
+        )
+        // .header(header)
+        .row_highlight_style(selected_row_style)
+        .column_highlight_style(selected_col_style)
+        .cell_highlight_style(selected_cell_style)
+        // .highlight_symbol(Text::from(vec![
+        //     "".into(),
+        //     bar.into(),
+        //     bar.into(),
+        //     "".into(),
+        // ]))
+        .bg(self.colors.buffer_bg)
+        .column_spacing(0);
+        // .highlight_spacing(HighlightSpacing::Always);
+        frame.render_stateful_widget(t, area, &mut self.state);
+    }
+
+    fn render_scrollbar(&mut self, frame: &mut Frame, area: Rect) {
+        frame.render_stateful_widget(
+            Scrollbar::default()
+                .orientation(ScrollbarOrientation::VerticalRight)
+                .begin_symbol(None)
+                .end_symbol(None),
+            area.inner(Margin {
+                vertical: 1,
+                horizontal: 1,
+            }),
+            &mut self.scroll_state,
+        );
+    }
+
+    fn render_header(&self, frame: &mut Frame, area: Rect) {
+        let header_style = Style::default()
+            .fg(self.colors.header_fg)
+            .bg(self.colors.header_bg);
+        let lay = Layout::vertical([
+            Constraint::Fill(3),
+            Constraint::Length(1),
+            Constraint::Max(2),
+        ])
+        .split(area);
+        frame.render_widget(
+            Paragraph::new("Soduku")
+                .style(header_style.add_modifier(Modifier::BOLD))
+                .centered(),
+            lay[1],
+        );
+    }
+    fn render_footer(&self, frame: &mut Frame, area: Rect) {
+        let info_footer = Paragraph::new(Text::from_iter(INFO_TEXT))
+            .style(
+                Style::new()
+                    .fg(self.colors.row_fg)
+                    .bg(self.colors.buffer_bg),
+            )
+            .centered()
+            .block(
+                Block::bordered()
+                    .border_type(BorderType::Double)
+                    .border_style(Style::new().fg(self.colors.footer_border_color)),
+            );
+        frame.render_widget(info_footer, area);
+    }
+}
+
+fn generate_fake_names() -> Vec<Data> {
+    use fakeit::{address, contact, name};
+
+    (1..=9)
+        .map(|_| {
+            let name = name::full();
+            let address = format!(
+                "{}\n{}, {} {}",
+                address::street(),
+                address::city(),
+                address::state(),
+                address::zip()
+            );
+            let email = contact::email();
+
+            Data {
+                name,
+                address,
+                email,
+                row: Arr9::default(),
             }
-            Event::Mouse(m) => self.handle_mouse_event(m),
-            _ => {}
-        };
-        Ok(())
-    }
+        })
+        .sorted_by(|a, b| a.name.cmp(&b.name))
+        .collect()
+}
 
-    fn exit(&mut self) {
-        self.exit = true;
-    }
+fn constraint_len_calculator(items: &[Data]) -> (u16, u16, u16) {
+    let name_len = items
+        .iter()
+        .map(Data::name)
+        .map(UnicodeWidthStr::width)
+        .max()
+        .unwrap_or(0);
+    let address_len = items
+        .iter()
+        .map(Data::address)
+        .flat_map(str::lines)
+        .map(UnicodeWidthStr::width)
+        .max()
+        .unwrap_or(0);
+    let email_len = items
+        .iter()
+        .map(Data::email)
+        .map(UnicodeWidthStr::width)
+        .max()
+        .unwrap_or(0);
 
-    fn decrement(&mut self) {
-        // self.counter = self.counter.saturating_sub(1);
-    }
-
-    fn increment(&mut self) {
-        // self.counter = self.counter.saturating_add(1);
-    }
+    #[allow(clippy::cast_possible_truncation)]
+    (name_len as u16, address_len as u16, email_len as u16)
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use crate::{Arr9, Data};
 
     #[test]
-    fn handle_key_event() {
-        let mut app = App::default();
-        app.handle_key_event(KeyCode::Right.into());
-        // assert_eq!(app.counter, 1);
+    fn constraint_len_calculator() {
+        let test_data = vec![
+            Data {
+                name: "Emirhan Tala".to_string(),
+                address: "Cambridgelaan 6XX\n3584 XX Utrecht".to_string(),
+                email: "tala.emirhan@gmail.com".to_string(),
+                row: Arr9::default(),
+            },
+            Data {
+                name: "thistextis26characterslong".to_string(),
+                address: "this line is 31 characters long\nbottom line is 33 characters long"
+                    .to_string(),
+                email: "thisemailis40caharacterslong@ratatui.com".to_string(),
+                row: Arr9::default(),
+            },
+        ];
+        let (longest_name_len, longest_address_len, longest_email_len) =
+            crate::constraint_len_calculator(&test_data);
 
-        app.handle_key_event(KeyCode::Left.into());
-        // assert_eq!(app.counter, 0);
-
-        let mut app = App::default();
-        app.handle_key_event(KeyCode::Char('q').into());
-        assert!(app.exit);
+        assert_eq!(26, longest_name_len);
+        assert_eq!(33, longest_address_len);
+        assert_eq!(40, longest_email_len);
     }
 }
